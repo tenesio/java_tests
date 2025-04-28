f60a63cd9b2cc2b12ef959ec56b92bec0c3f6fab
tenesio <luis.avelis.paula@gmail.com>
2025-04-28 07:33:23 -0300

    banco

--- bankSystem.java
+++ bankSystem.java
@@ -1,8 +1,9 @@
 import java.util.ArrayList;
 import java.util.Scanner;
+
 public class bankSystem {
     ArrayList<Account> contas = new ArrayList<Account>();
-    
+
     public static void main(String[] args) {
         int op = 0;
         Scanner scan = new Scanner(System.in);
@@ -13,8 +14,8 @@ public class bankSystem {
         }
         scan.close();
     }
-    public static void Menu()
-    {
+
+    public static void Menu() {
         System.out.println("1 - criar conta");
         System.out.println("2 - alterar conta");
         System.out.println("3 - listar todas as contas");
@@ -25,9 +26,31 @@ public class bankSystem {
         System.out.println("8 - verificar modificações");
         System.out.println("9 - ");
     }
-    public static void opMenu(int op)
-    {
 
+    public static void opMenu(int op) {
+        switch (op) {
+            case 1:
+                System.out.println("Criar Conta");
+                criar_conta();
+                break;
+            case 2:
+                System.out.println("Alterar Conta");
+                // Altera_conta();
+                break;
+            case 3:
+                System.out.println("Lista de Contas");
+                // listar_contas();
+                break;
+            case 4:
+                System.out.println("");
+            default:
+                break;
+        }
     }
-    
+
+    public static Account criar_conta() {
+        Account account = new Account(" ", "", "", 0, "", "", "");
+
+        return account;
+    }
 }
